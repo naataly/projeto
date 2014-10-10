d88413d9adf35fd1c29899962cfdf0a08fa0508f
naataly <natalycristin48@gmail.com>
2014-10-10 20:50:27 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,58 @@
+projeto
+=======
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Walloween</title>       
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>    
+    </head>
+    <body>        
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Projeto</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Login</button>
+          </form>
+		  <img src="img/wall.jpg" />
+		  <p>Em declaração feita no ano de 2009, o Vaticano condenou o Halloween como uma festa perigosa carregada por vários elementos anticristãos. No Brasil, observamos que algumas pessoas torcem o nariz para a comemoração do evento por entendê-lo como uma manifestação distante da nossa cultura. No fim das contas, muito se diz a respeito, mas poucos são aqueles que examinam minuciosamente os significados e origens de tal festividade.
+
+Desde a Antiguidade, observamos que várias festividades populares eram cercadas pela valorização dos opostos que regem o mundo. Um dos mais claros exemplos disso ocorre com relação ao carnaval, que antecede toda a resignação da quaresma. No caso do Halloween, desde muito tempo, a festividade acontece um dia antes da “festa de todos os santos” e, por isso, tem seu nome inspirado na expressão "All hallow's eve", que significa a “véspera de todos os santos”.
+
+Pelo fato do 1° de novembro estar cercado de um valor sagrado e extremamente positivo, os celtas, antigo povo que habitava as Ilhas Britânicas, acreditavam que o mundo seria ameaçado na véspera do evento pela ação de terríveis demônios e fantasmas. Dessa forma, o “halloween” nasce como uma preocupação simbólica onde a festa cercada por figuras estranhas e bizarras teria o objetivo de afastar a influência dos maus espíritos que ameaçariam suas colheitas.
+
+No processo de ocupação das terras europeias, os povos pagãos trouxeram esta influencia cultural em pleno processo de disseminação do cristianismo. Inicialmente, os cristãos celebravam a todos os santos no mês de maio. Contudo, por volta do século IX, a Igreja promoveu uma adaptação em que a festa sagrada fora deslocada para o 1° de novembro. Dessa forma, os bárbaros convertidos se lembrariam da festa cristã que sucederia a antiga e já costumeira celebração do halloween.
+
+Por ter essa relação intrínseca ao mundo dos espíritos, o halloween foi logo associado à figura das bruxas e feiticeiras. Na Idade Média, elas se tornaram ainda mais recorrentes na medida em que a Inquisição perseguiu e acusou várias pessoas de exercerem a bruxaria. Da mesma forma, os mortos também se tornaram comuns nesta celebração, por não mais pertencerem a essa mesma realidade etérea.
+
+Entre todos os desalmados, destaca-se a antiga lenda de Stingy Jack. Segundo o mito irlandês, ele teria convidado o Diabo para beber com ele no dia do Halloween. Após se fartarem em bebida, o astuto Jack convenceu o Diabo a se transformar em uma moeda para que a conta do bar fosse paga. Contudo, ao invés de saldar a dívida, Jack pregou a moeda em um crucifixo.
+
+Para se livrar da prisão, o Diabo aceitou um acordo em que prometia nunca importunar Jack. Dessa forma, ele foi libertado e nunca mais importunou o homem. Entretanto, Jack morreu e não foi aceito nas portas do céu por ter realizado um trato com o demônio. Ao descer para os infernos, também foi rejeitado pelo Diabo por conta do trato que possuíam. Vendo que Jack estava solitário e perdido, o demônio lhe entregou um nabo com carvão que lhe serviu de lanterna.
+
+Ao chegarem à América do Norte, os irlandeses trouxeram a festa do Halloween para as Américas e transformaram a lanterna de Jack em uma abóbora iluminada com feições humanas. Os disfarces e máscaras, tão usadas pelos participantes da festa, seriam uma forma de evitar que fossem reconhecidos pelos espíritos que vagam neste dia. Atualmente, as fantasias são utilizadas por crianças que batem às portas exigindo guloseimas no lugar de alguma travessura contra o proprietário da casa.
+
+De fato, a celebração do Halloween remete a uma série de antigos valores da cultura bárbaro-cristã que se forma na Europa Medieval. Nessa época, várias outras festas celebravam o processo de movimentação do mundo ao destacar os opostos que configuravam o seu mundo. No jogo de oposições simbólicas, mais do que o valor de um simples embate, o homem acaba por visualizar a alternância e a transformação enquanto elementos centrais da vida</p>
+        </div>
+      </div>
+    </div>
